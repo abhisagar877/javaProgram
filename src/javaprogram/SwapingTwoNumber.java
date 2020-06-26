63933e2dd7ffea726e135ec0400e785ee480e3e0
Abhishek <abhisagar87@gmail.com>
2020-06-26 08:51:14 +0530

    Second logic

--- src/javaprogram/SwapingTwoNumber.java
+++ src/javaprogram/SwapingTwoNumber.java
@@ -6,10 +6,17 @@ public class SwapingTwoNumber {
 		int a=10 , b=20;
 		System.out.println("before Swaping the value are.."+a+" "+b);
 		
-		//Logical 1-Using thirvariable 
-		int t=a;
+		//Logical 1-Using third variable 
+		/*int t=a;
 		a=b;
 		b=t;
+		*/
+		//Second logic
+		a=a+b;
+		b=a-b;
+		a=a-b;
 		System.out.println("After Swaping the value are.."+a+" "+b);
+		
+		//Third logic 
 	}
 }
